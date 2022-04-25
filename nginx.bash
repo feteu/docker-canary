9919f78a3aad5e06c1b2b98b79011201086fc3c7
feteu <fabiogreco@lightline.ch>
2022-04-25 09:44:15 +0200

    resetting weight in nginx.conf

--- nginx.bash
+++ nginx.bash
@@ -1,7 +1,7 @@
 docker run \
     --detach \
     --name "nginx" \
-    --network "docker-blue-green" \
+    --network "docker-canary" \
     --publish "127.0.0.1:8888:8888" \
     --volume "$(pwd)/nginx/:/etc/nginx/conf.d:ro" \
     nginx:latest
